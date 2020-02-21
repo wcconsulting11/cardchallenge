1186eba87f270a896e09ef79faf86b887b8f7977
Wendy Cordero <wcconsulting11@gmail.com>
2020-02-21 23:26:10 +0000

    initial commit

---
+++ cardchallenge.rb
@@ -0,0 +1,41 @@
+class Card
+    attr_reader :rank, :suit
+
+    def initialize(rank,suit)
+        @rank = rank
+        @suit = suit
+    end
+
+
+    def output_card
+        puts "The #{@rank} of #{@suit}"
+    end  
+end
+
+
+class Deck    
+    def initialize
+        @ranks = [*(2..10),'J','Q','k','A']
+        @suits = ['Hearts', 'Spades', 'Diamonds', 'Club']
+        @cards = []
+
+        @ranks.each do |rank|
+        @suits.each do |suit|
+        @cards << Card.new(rank,suit)
+
+    end
+end
+    
+
+@cards.shuffle!
+end
+
+
+    def deal (number)
+        number.times{@cards.shift.output_card}
+    end
+end
+   
+ 
+deck = Deck.new    
+deck.deal (5)
